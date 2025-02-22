8a1f2edaa0b0852aba02a9fbaa553dc607958743
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-02-23 02:58:31 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,45 +11,47 @@ import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 import { LoginOverlay } from "./LoginOverlay.tsx";
 import { Agent } from "@atproto/api";
-
-const client = await BrowserOAuthClient.load({
-	clientId: "https://skytools.womo-x.win/client-metadata.json",
-	handleResolver: "https://public.api.bsky.app",
-});
-const { session, agent } = await (async (): Promise<{ session?: OAuthSession; agent?: Agent }> => {
-	const result: undefined | { session: OAuthSession; state?: string | null } = await client.init();
-	if (result != null) {
-		const { session, state } = result;
-		const agent = new Agent(session);
-		return { session, agent };
-	}
-	const lastDid = localStorage.getItem("did");
-	if (lastDid == null) return {};
-	try {
-		const session = await client.restore(lastDid);
-		const agent = new Agent(session);
-		localStorage.setItem("did", session.did);
-		return { session, agent };
-	} catch (e) {
-		return {};
-	}
-})();
-const login = (handle: string) => client.signIn(handle);
-const logout = async () => {
-	await session?.signOut();
-	localStorage.removeItem("did");
-	location.href = "/";
+const init = async () => {
+	const client = await BrowserOAuthClient.load({
+		clientId: "https://skytools.womo-x.win/client-metadata.json",
+		handleResolver: "https://public.api.bsky.app",
+	});
+	const { session, agent } = await (async (): Promise<{ session?: OAuthSession; agent?: Agent }> => {
+		const result: undefined | { session: OAuthSession; state?: string | null } = await client.init();
+		if (result != null) {
+			const { session, state } = result;
+			const agent = new Agent(session);
+			return { session, agent };
+		}
+		const lastDid = localStorage.getItem("did");
+		if (lastDid == null) return {};
+		try {
+			const session = await client.restore(lastDid);
+			const agent = new Agent(session);
+			localStorage.setItem("did", session.did);
+			return { session, agent };
+		} catch (e) {
+			return {};
+		}
+	})();
+	const login = (handle: string) => client.signIn(handle);
+	const logout = async () => {
+		await session?.signOut();
+		localStorage.removeItem("did");
+		location.href = "/";
+	};
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	createRoot(document.getElementById("root")!).render(
+		<StrictMode>
+			<BrowserRouter>
+				<CssBaseline />
+				<Routes>
+					<Route index element={<Index />} />
+					<Route path="*" element={<App {...{ agent: agent ?? null, login, logout }} />} />
+				</Routes>
+				<LoginOverlay.Root />
+			</BrowserRouter>
+		</StrictMode>,
+	);
 };
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<BrowserRouter>
-			<CssBaseline />
-			<Routes>
-				<Route index element={<Index />} />
-				<Route path="*" element={<App {...{ agent: agent ?? null, login, logout }} />} />
-			</Routes>
-			<LoginOverlay.Root />
-		</BrowserRouter>
-	</StrictMode>,
-);
+init();
